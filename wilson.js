28f0b5cc97980b3894969dc85b3b28d706f77928
Ryan Wilson <rtwilson@stanford.edu>
2012-12-19 15:06:46 -0800

    Fix formatting

--- wilson.js
+++ wilson.js
@@ -1,22 +1,28 @@
 var current_hash;
 
-function change_hash(hash) {
-    if (hash != current_hash) {
+function change_hash(hash)
+{
+    if (hash != current_hash)
+    {
         var el = document.getElementById(hash);
-        if (el) {
+        if (el)
+	{
             var content = document.getElementById("content");
-            for (var i = 0; i < content.childNodes.length; i++) {
-				var node = content.childNodes[i];
+            for (var i = 0; i < content.childNodes.length; i++)
+	    {
+		var node = content.childNodes[i];
                 if ("style" in node)
+		{
                     node.style.display = "none";
+		}
             }
             el.style.display = "block";
-			current_hash = hash;
+	    current_hash = hash;
         }
     }
 }
 
 window.onload = function() {
     var hash = (window.location.hash || "#home").substr(1);
-	change_hash(hash);
+    change_hash(hash);
 }
